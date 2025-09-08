73ccc56756166dc86145c856eac605e87ea7ea0c
ChristianJacobMahinay-UCM <christianjacob1001@gmail.com>
2025-09-08 10:09:38 +0800

    Create FixDebugOne.java

---
+++ Chapter_1/FixDebugOne.java
@@ -0,0 +1,18 @@
+public class FixDebugOne
+{
+    /* This program displays a greeting */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
+
+// 1. The class name FixDebug One should not have a space between the two words. It should be FixDebugOne or FixDebug_One.
+
+// 2. The Main method's signature should be public static void main(String[] args), with main in lowercase and args being an array of String (i.e., String[] args).
+
+// 3. Systm should be corrected to System.
+
+// 4. You are missing a semicolon (;) at the end of the line with System.out.println.
+
+// 5. The comment block is not properly closed with */.

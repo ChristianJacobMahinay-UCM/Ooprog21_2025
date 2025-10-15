c85dadfb041aee80caed00da7b4039d48afbd4ac
ChristianJacobMahinay-UCM <christianjacob1001@gmail.com>
2025-10-16 01:13:21 +0800

    Update DebugFive2.java

--- Chapter_6/DebugFive2.java
+++ Chapter_6/DebugFive2.java
@@ -6,13 +6,13 @@ public class DebugFive2 {
         int num2;
         Scanner input = new Scanner(System.in);
 
-        // Prompt user for input
+    
         System.out.print("Enter a number: ");
         num = input.nextInt();  // Corrected method call
         System.out.print("Enter another number: ");
         num2 = input.nextInt();  // Corrected method call
 
-        // Check if either number is divisible by the other
+    
         if (num % num2 == 0 || num2 % num == 0) {  // Corrected condition
             System.out.println("One of these numbers is");
             System.out.println(" evenly divisible into the other");
